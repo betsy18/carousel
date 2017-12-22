dadcfd9f95cc831b0799a5e63543d391726d44ef
betsy18 <betsy_dessire@hotmail.com>
2017-12-22 03:00:58 -0500

    Subiendo cambios de js

--- js/app.js
+++ js/app.js
@@ -9,8 +9,8 @@ function loadPage() {
 
   //   eventos
   $buttons.click(changeImage);
-  $previus.click(previusImage);
-  $next.click(nextImage);
+  $previus.keypress(previusImage);
+  $next.keypress(nextImage);
 }
 
 function changeImage() {
@@ -27,7 +27,7 @@ function previusImage(e) {
 
 function nextImage(e) {
   e.preventDefault();
-  target = target + 1;
+  target = target - 1;
   target = (target > 4) ? 0 : target;
   showImage(target);
 }
